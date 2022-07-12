6d4fb7fe7b1d63fba7d5a28cd1daa9433c1b82a7
jon-harley <jon_oliveira@outlook.com>
2022-07-12 18:59:02 -0300

    criação do perfil de DEV

---
+++ src/main/java/com/jonathan/bookstore/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.jonathan.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.jonathan.bookstore.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	public String strategy;
+	
+	@Bean
+	public boolean instanciaBaseDeDados() {
+		if(strategy.equals("create")) {
+			this.dbService.instanciaBaseDeDados();
+		}
+		return false;
+	}
+}
