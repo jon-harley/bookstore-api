b358411862ee9bbeb0ffdd131961a2ae09518af2
jon-harley <jon_oliveira@outlook.com>
2022-07-12 15:12:12 -0300

    repositories implementados

---
+++ src/main/java/com/jonathan/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.jonathan.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jonathan.bookstore.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
