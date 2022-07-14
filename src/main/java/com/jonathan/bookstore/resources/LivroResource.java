e5792e1abc92bc6d4fc3087304157bf74bd5385d
jon-harley <jon_oliveira@outlook.com>
2022-07-14 00:13:11 -0300

    PUT e PATCH para Livro

--- src/main/java/com/jonathan/bookstore/resources/LivroResource.java
+++ src/main/java/com/jonathan/bookstore/resources/LivroResource.java
@@ -6,7 +6,10 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,9 +32,11 @@ public class LivroResource {
 	}
 
 	// @GetMapping(value = "/category/{id}")
-	// public ResponseEntity<List<LivroDTO>> findAllByCategory(@PathVariable Integer id)
+	// public ResponseEntity<List<LivroDTO>> findAllByCategory(@PathVariable Integer
+	// id)
 	@GetMapping
-	public ResponseEntity<List<LivroDTO>> findAllByCategory(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat) {
+	public ResponseEntity<List<LivroDTO>> findAllByCategory(
+			@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat) {
 		List<Livro> list = service.findAllByCategory(id_cat);
 		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
@@ -43,5 +48,17 @@ public class LivroResource {
 		List<Livro> list = service.findAll();
 		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj) {
+		Livro newObj = service.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj) {
+		Livro newObj = service.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
 	}
 }
