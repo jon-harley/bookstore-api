c4462d71ce2d2db079117d07a626be88e965dce0
jon-harley <jon_oliveira@outlook.com>
2022-07-14 01:46:57 -0300

    Ajustes finalizados para o backend

--- src/main/java/com/jonathan/bookstore/dtos/CategoriaDTO.java
+++ src/main/java/com/jonathan/bookstore/dtos/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.jonathan.bookstore.dtos;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.jonathan.bookstore.domain.Categoria;
 
 import lombok.Data;
@@ -10,9 +14,14 @@ import lombok.Data;
 public class CategoriaDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
+	@NotEmpty(message = "Campo NOME é requerido")
+	@Length(min = 3, max = 100, message = "O campo NOME deve ter entre 3 e 100 caracteres")
 	private String nome;
+
+	@NotEmpty(message = "Campo DESCRIÇÃO é requerido")
+	@Length(min = 3, max = 200, message = "O campo DESCRIÇÃO deve ter entre 3 e 200 caracteres")
 	private String descricao;
 
 	public CategoriaDTO() {
