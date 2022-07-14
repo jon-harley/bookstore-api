ada4e51dc5ea4057ab7a1f4e3794aedf64e0f7ef
jon-harley <jon_oliveira@outlook.com>
2022-07-14 00:33:04 -0300

    POST para Livro

--- src/main/java/com/jonathan/bookstore/service/LivroService.java
+++ src/main/java/com/jonathan/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jonathan.bookstore.domain.Categoria;
 import com.jonathan.bookstore.domain.Livro;
 import com.jonathan.bookstore.repositories.LivroRepository;
 import com.jonathan.bookstore.service.exceptions.ObjectNotFoundException;
@@ -46,5 +47,12 @@ public class LivroService {
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
 	}
-	
+
+	public Livro create(Integer id_cat,Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
+	}
+
 }
